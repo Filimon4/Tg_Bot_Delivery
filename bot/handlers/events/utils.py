bb8d72e4ecd035d89b54246ca3fe1d1f8aa56068
Ar524 <borisovseva7@gmail.com>
2023-01-19 14:40:04 +0300

    state_machine

--- bot/handlers/events/utils.py
+++ bot/handlers/events/utils.py
@@ -2,12 +2,7 @@ from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
-
-
 from ..button import *
-
-
-
 
 async def start(msg: types.Message):
     await msg.answer("Здравствуйте, вас приветсвует бот для заказов еды", reply_markup=reply_key.kb_menu)
